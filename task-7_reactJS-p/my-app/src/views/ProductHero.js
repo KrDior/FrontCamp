87f55b5cc673525184780aee9114a7a337401818
KrDior <xzx89s@gmail.com>
2019-12-10 00:52:51 +0300

    so tired

--- task-7_reactJS-p/my-app/src/views/ProductHero.js
+++ task-7_reactJS-p/my-app/src/views/ProductHero.js
@@ -1,6 +1,12 @@
-import React from 'react';
+/* eslint-disable react/forbid-prop-types */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import Button from '../components/Button';
 import Typography from '../components/Typography';
 import ProductHeroLayout from './ProductHeroLayout';
@@ -16,6 +22,10 @@ const styles = (theme) => ({
   button: {
     minWidth: 200,
   },
+  buttonGroup: {
+    width: 100,
+    maxHeight: 50,
+  },
   h5: {
     marginBottom: theme.spacing(2),
     marginTop: theme.spacing(2),
@@ -26,52 +36,99 @@ const styles = (theme) => ({
   more: {
     marginTop: theme.spacing(2),
   },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 600,
+  },
+  input: {
+    fontSize: 20,
+  },
+  searchText: {
+    marginLeft: 75,
+    marginRight: 5,
+  },
 });
 
 function ProductHero(props) {
   const { classes } = props;
+  const [inputValue, setInputValue] = useState('');
+  const [alignment, setAlignment] = React.useState('left');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setInputValue('');
+  };
 
   return (
     <ProductHeroLayout backgroundClassName={classes.background}>
       {/* Increase the network loading priority of the background image. */}
-      <img
-        style={{ display: 'none' }}
-        src={backgroundImageStatic}
-        alt="increase priority"
-      />
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h3"
-        marked="center"
-      >
-                Find your favorite movie
+      <img style={{ display: 'none' }} src={backgroundImageStatic} alt="increase priority" />
+      <Typography color="inherit" align="center" variant="h3" marked="center">
+        Find your favorite movie
       </Typography>
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
-        className={classes.h5}
-      >
-                Enjoy secret offers up to -70% off the best films catalogue every
-                Sunday.
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        Enjoy secret offers up to -70% off the best films catalogue every Sunday.
       </Typography>
-      <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        className={classes.button}
-        component="a"
-        href="/premium-themes/onepirate/sign-up/"
-      >
-                Register
-      </Button>
-      <Typography
-        variant="body2"
-        color="inherit"
-        className={classes.more}
-      >
-                Discover the experience
+      <Grid container spacing={1} direction="row" alignItems="center" justify="flex-end">
+        <Grid item xs={6}>
+          <span className={classes.searchText}>SEARCH BY</span>
+          <ToggleButtonGroup
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+            size="small"
+          >
+            <ToggleButton value="left" aria-label="left aligned">
+              Title
+            </ToggleButton>
+            <ToggleButton value="right" aria-label="right aligned">
+              Gengere
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Grid>
+      </Grid>
+      <Grid container spacing={1} direction="column" alignItems="center" justify="center">
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-full-width"
+            className={clsx(classes.textField, classes.input)}
+            label="Movie name"
+            style={{ margin: 8 }}
+            placeholder="Let's try to find..."
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+            color="secondary"
+            value={inputValue}
+            onChange={(evt) => setInputValue(evt.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            color="secondary"
+            variant="contained"
+            size="large"
+            className={classes.button}
+            component="a"
+            href="/premium-themes/onepirate/sign-up/"
+            onClick={handleSubmit}
+          >
+            <span>Find movie</span>
+          </Button>
+        </Grid>
+      </Grid>
+
+      <Typography variant="body2" color="inherit" className={classes.more}>
+        Discover the experience
       </Typography>
     </ProductHeroLayout>
   );
